7494192e52d5d4fbf5ce114cec73021d79aadb66
nguyentrucan <an.ngtruc2002@gmail.com>
2025-10-23 17:04:46 +0700

    Upate logo, MainLayout and ex Home page

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 import { NavLink, Outlet } from "react-router-dom";
 
 const navLink =
-  "px-2 py-2 rounded-lg hover-bg-gray-100 dark:hover:bg-gray-800 transition";
+  "px-2 py-2 rounded-lg hover:text-white hover-bg-gray-100 dark:hover:bg-gray-800 transition";
 
 export default function MainLayout() {
   return (
@@ -9,7 +9,7 @@ export default function MainLayout() {
       <header className="border-b">
         <div className="container flex items-center justify-between h-16">
           <a href="/" className="font-bold text-xl">
-            amtg<span className="text-brand">.studio</span>
+            Ảnh màu<span className="text-brand"> Trường Giang</span>
           </a>
           <nav className="flex gap-2">
             <NavLink to={"/"} className={navLink} end>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-    return (
-        <div>
-          Home  
-        </div>
-    );
+  return (
+    <section className="grid gap-6">
+      <h1 className="text-3xl font-bold">
+        Chào mừng đến với{" "}
+        <span className="text-brand">Ảnh màu Trường Giang</span>
+      </h1>
+    </section>
+  );
 };
 
 export default Home;
